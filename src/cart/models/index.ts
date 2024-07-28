e29e32de7ca42c91e23bfec8d8869c19e28686b2
baranovdv <baranov.d.v.1988@gmail.com>
2024-07-28 23:47:42 +0500

    init: init cdk deploy

--- src/cart/models/index.ts
+++ src/cart/models/index.ts
@@ -18,9 +18,9 @@ export type CartItem = {
 
 export type Cart = {
   id: string,
-  user_id: string,
-  created_at: string,
-  updated_at: string,
-  status: CartStatuses,
+  user_id?: string,
+  created_at?: string,
+  updated_at?: string,
+  status?: CartStatuses,
   items: CartItem[],
 }
