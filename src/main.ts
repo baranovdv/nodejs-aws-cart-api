e0876fe7a80779f17868aa790eb39a7e880a620e
baranovdv <baranov.d.v.1988@gmail.com>
2024-08-06 02:23:10 +0500

    refactor: refactor server start

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,46 @@
 import { NestFactory } from '@nestjs/core';
-import serverlessExpress from '@codegenie/serverless-express';
-import { Callback, Context, Handler } from 'aws-lambda';
+
+import helmet from 'helmet';
+
 import { AppModule } from './app.module';
 
-let server: Handler;
+const port = process.env.PORT || 4000;
 
-async function bootstrap(): Promise<Handler> {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.init();
 
-  const expressApp = app.getHttpAdapter().getInstance();
-  return serverlessExpress({ app: expressApp });
+  app.enableCors({
+    origin: (req, callback) => callback(null, true),
+  });
+  app.use(helmet());
+
+  await app.listen(port);
 }
+bootstrap().then(() => {
+  console.log('App is running on %s port', port);
+});
 
-export const handler: Handler = async (
-  event: any,
-  context: Context,
-  callback: Callback,
-) => {
-  server = server ?? (await bootstrap());
-  return server(event, context, callback);
-};
+// import { NestFactory } from '@nestjs/core';
+// import serverlessExpress from '@codegenie/serverless-express';
+// import { Callback, Context, Handler } from 'aws-lambda';
+// import { AppModule } from './app.module';
+
+// let server: Handler;
+
+// async function bootstrap(): Promise<Handler> {
+//   const app = await NestFactory.create(AppModule);
+//   app.enableCors();
+//   await app.init();
+
+//   const expressApp = app.getHttpAdapter().getInstance();
+//   return serverlessExpress({ app: expressApp });
+// }
+
+// export const handler: Handler = async (
+//   event: any,
+//   context: Context,
+//   callback: Callback,
+// ) => {
+//   server = server ?? (await bootstrap());
+//   return server(event, context, callback);
+// };
